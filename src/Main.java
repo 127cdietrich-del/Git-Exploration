c5fe24319dd97f131f8b97767288205cab25fb92
127CFusco <127cfusco@frhsd.com>
2025-09-08 10:35:54 -0400

    i say hi also

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hi my name is Christian");
+        System.out.println("Hi Christian, my name is CARSON");
     }
 }
